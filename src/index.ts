d56e64ca8021389a8a47432176ead8e97890b3cb
Chinmoy Kalita <chinmoykalita210@gmail.com>
2023-12-31 00:28:41 +0530

    added user manager

--- src/index.ts
+++ src/index.ts
@@ -13,16 +13,10 @@ httpServer.listen(8000, () => {
 
 const wsServer = new WebSocketServer({
     httpServer: httpServer,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
     autoAcceptConnections: false
 });
 
 function originIsAllowed(origin: any) {
-  // put logic here to detect whether the specified origin is allowed.
   return true;
 }
 
@@ -36,6 +30,7 @@ wsServer.on('request', function(request) {
     
     var connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
+    
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
             console.log('Received Message: ' + message.utf8Data);
